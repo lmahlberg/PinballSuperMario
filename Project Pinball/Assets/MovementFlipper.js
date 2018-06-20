3eb81b6d136d36f01492fa6c84f6f2d57fdda5c1
Lauri <laura.mahlberg1201@googlemail.com>
2018-06-20 19:14:32 +0200

    Add files via upload

---
+++ Project Pinball/Assets/MovementFlipper.js
@@ -0,0 +1,9 @@
+#pragma strict
+
+var myInt : int = 5;
+
+function MyFunction (number : int) : int
+{
+    var ret = myInt * number;
+    return ret;
+}
---
+++ Project Pinball/Assets/movement.cs
@@ -0,0 +1,32 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+
+    public float restPosition = 0f;
+	public float pressPosition = 45f;
+	public float hitstrenght = 10000f;
+	public float flipperDamper = 150f;
+	HingeJoint hinge;
+
+
+	public string InputName; 
+	// Use this for initialization
+	void Start () {
+		hinge.GetComponent<HingeJoint();
+		hinge.useSpring = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetAxis(InputName) == 1){
+
+
+		}
+	}
+}*/
+#pragma Strict
+
